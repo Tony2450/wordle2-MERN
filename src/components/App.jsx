7359cff28eecfd08d58dbe163572bc87e1127bc7
unknown <afanous@hotmail.com>
2023-10-19 16:26:11 -0400

    Updated react router to use v6.4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,12 @@ import { AppProvider, useApp } from "./RealmApp";
 import atlasConfig from "../atlasConfig.json";
 import "./App.css";
 import Wordle from "./Wordle";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import ProtectedLayout from "./ProtectedLayout";
 import Layout from "./Layout";
 import Rules from "./Rules";
@@ -19,21 +24,25 @@ export default function ProvidedApp() {
   );
 }
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="/wordle2-MERN/user" element={<ProtectedLayout />}>
+        <Route path="wordle-clone" element={<Wordle />} />
+        <Route path="rules" element={<Rules />} />
+        <Route path="userstats" element={<UserStatsPage />} />
+      </Route>
+      <Route path="/wordle2-MERN" element={<Layout />}>
+        <Route path="/wordle2-MERN" element={<WelcomePage />} />
+        <Route path="rules" element={<Rules />} />
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
   const { currentUser, logOut } = useApp();
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/wordle2-MERN/user" element={<ProtectedLayout />}>
-          <Route path="wordle-clone" element={<Wordle />} />
-          <Route path="rules" element={<Rules />} />
-          <Route path="userstats" element={<UserStatsPage />} />
-        </Route>
-        <Route path="/wordle2-MERN" element={<Layout />}>
-          <Route path="/wordle2-MERN" element={<WelcomePage />} />
-          <Route path="rules" element={<Rules />} />
-        </Route>
-      </Routes>
-    </BrowserRouter >
+    <RouterProvider router={router} />
   );
 }
